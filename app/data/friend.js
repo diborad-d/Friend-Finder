d8e19d2ad23e5b6eb29a850af9cf2c60e5e38a8e
Dibora D <diborad@yahoo.com>
2019-10-23 17:30:08 -0500

    set up node modules and front end frameworks

---
+++ app/data/friend.js
@@ -0,0 +1,33 @@
+// You should save your application's data inside of app/data/friends.js as an array of objects. Each of these objects should roughly follow the format below.
+
+// {
+//   "name":"Sarah Noah",
+//   "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+//   "scores":[
+//       5,
+//       1,
+//       4,
+//       4,
+//       5,
+//       1,
+//       2,
+//       5,
+//       4,
+//       1
+//     ]
+// }
+
+// USE Math.abs /REDUCE to find the total differences aka campatability. 
+var userArray = [
+    {
+      firstName: "John",
+      lastName: "Smith",
+      userEmail: "ahmed@example.com",
+      userImage: "https://bit.ly/31IbbkS",
+      phoneNumber: "000-000-0000"
+    }
+  ];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = userArray;
+  
---
+++ routing/apiRoutes.js
@@ -0,0 +1,30 @@
+// apiRoutes.js file should contain two routes:
+var tableData = require("../data/friendsData");
+var waitListData = require("../data/surveyData");
+
+// ROUTING
+module.exports = function(app) {
+  app.get("/api/friends", function(req, res) {
+    res.json(tableData);
+  });
+  app.get("/api/survey", function(req, res) {
+    res.json(waitListData);
+  });
+  // A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+  app.post("/api/friends", function(req, res) {
+    if (tableData.length < 5) {
+      tableData.push(req.body);
+      res.json(true);
+    } else {
+      waitListData.push(req.body);
+      res.json(false);
+    }
+  });
+  app.post("/api/clear", function(req, res) {
+    // Empty out the arrays of data
+    tableData.length = 0;
+    waitListData.length = 0;
+
+    res.json({ ok: true });
+  });
+};
