d6feb7fa75448d20642f8cfb2bd9fe7380f91f5c
Dibora D <diborad@yahoo.com>
2020-06-23 13:27:08 -0500

    updared data

--- app/data/friend.js
+++ app/data/friend.js
@@ -1,66 +1,66 @@
 var userArray = [
   {
-    Name: "John Smith",
+    Name: "Alan Turing",
     userEmail: "john@example.com",
-    userImage: "https://bit.ly/31IbbkS",
+    userImage: "https://www.biography.com/scientist/alan-turing",
     phoneNumber: "000-000-0000",
     scores: ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"]
   },
   {
-    Name: "Sarah McDonalds",
+    Name: "Malala Yousafzai ",
     userEmail: "msarah@example.com",
     userImage: "https://bit.ly/31IbbkS",
     phoneNumber: "000-000-0000",
-    scores: ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"]
+    scores: ["2", "1", "4", "1", "3", "2", "5", "4", "1", "4"]
+  },
+  {
+    Name: "Ludwig van Beethoven",
+    userEmail: "john@example.com",
+    userImage: "biography.com/musician/ludwig-van-beethoven",
+    phoneNumber: "000-000-0000",
+    scores: ["1", "2", "4", "2", "4", "1", "2", "5", "2", "1"]
+  },
+  {
+    Name: "RuPaul",
+    userEmail: "john@example.com",
+    userImage: "https://www.biography.com/personality/rupaul",
+    phoneNumber: "000-000-0000",
+    scores: ["3", "1", "3", "2", "4", "3", "2", "2", "5", "3"]
+  },
+  {
+    Name: "Kendrick Lamar ",
+    userEmail: "john@example.com",
+    userImage: "https://www.biography.com/musician/kendrick-lamar",
+    phoneNumber: "000-000-0000",
+    scores: ["4", "5", "2", "2", "4", "4", "2", "2", "4", "3"]
+  },
+  {
+    Name: "Jimmy Kimmel",
+    userEmail: "john@example.com",
+    userImage: "https://www.biography.com/media-figure/jimmy-kimmel",
+    phoneNumber: "000-000-0000",
+    scores: ["2", "3", "4", "5", "2", "5", "3", "4", "5", "1"]
+  },
+  {
+    Name: "Michelle Williams",
+    userEmail: "john@example.com",
+    userImage: "https://www.biography.com/actor/michelle-williams",
+    phoneNumber: "000-000-0000",
+    scores: ["2", "4", "5", "2", "2", "4", "5", "3", "4", "5"]
   },
   {
     Name: "John Smith",
     userEmail: "john@example.com",
     userImage: "https://bit.ly/31IbbkS",
     phoneNumber: "000-000-0000",
-    scores: ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"]
+    scores: ["1", "3", "5", "3", "3", "4", "4", "4", "3", "2"]
   },
   {
-    Name: "John Smith",
+    Name: "Paul McCartney",
     userEmail: "john@example.com",
-    userImage: "https://bit.ly/31IbbkS",
+    userImage: "https://www.biography.com/musician/paul-mccartney",
     phoneNumber: "000-000-0000",
-    scores: ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"]
-  },
-  {
-    Name: "John Smith",
-    userEmail: "john@example.com",
-    userImage: "https://bit.ly/31IbbkS",
-    phoneNumber: "000-000-0000",
-    scores: ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"]
-  },
-  {
-    Name: "John Smith",
-    userEmail: "john@example.com",
-    userImage: "https://bit.ly/31IbbkS",
-    phoneNumber: "000-000-0000",
-    scores: ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"]
-  },
-  {
-    Name: "John Smith",
-    userEmail: "john@example.com",
-    userImage: "https://bit.ly/31IbbkS",
-    phoneNumber: "000-000-0000",
-    scores: ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"]
-  },
-  {
-    Name: "John Smith",
-    userEmail: "john@example.com",
-    userImage: "https://bit.ly/31IbbkS",
-    phoneNumber: "000-000-0000",
-    scores: ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"]
-  },
-  {
-    Name: "John Smith",
-    userEmail: "john@example.com",
-    userImage: "https://bit.ly/31IbbkS",
-    phoneNumber: "000-000-0000",
-    scores: ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"]
+    scores: ["2", "3", "3", "2", "3", "2", "2", "1", "1", "2"]
   }
 ];
 
